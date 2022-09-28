6c3d7a5ec7c0af19eebaba6fe81fcd9e701a712e
Scy_c <2402265378@qq.com>
2022-09-28 09:46:58 +0800

    2

---
+++ src/main/java/com/example/emo/wx/EmoWxApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.emo.wx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoWxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmoWxApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emo/wx/EmoWxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emo.wx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoWxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
